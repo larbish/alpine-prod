9ae53b7f0585e136773b2d7c40378b7cab417775
Baptiste Leproux <leproux.baptiste@gmail.com>
2023-06-27 17:44:43 +0200

    Squash feat/new into main

--- app.config.ts
+++ app.config.ts
@@ -37,5 +37,12 @@ export default defineAppConfig({
     form: {
       successMessage: 'Message sent. Thank you!'
     }
+  },
+
+  nuxtIcon: {
+    aliases: {
+      key-244: '',
+      key-106: ''
+    }
   }
 })
