325e552afb2b6841c3e47502b61d0d78c083ce3e
Baptiste Leproux <leproux.baptiste@gmail.com>
2023-06-28 17:49:41 +0200

    Update app.config.ts

--- app.config.ts
+++ app.config.ts
@@ -37,12 +37,5 @@ export default defineAppConfig({
     form: {
       successMessage: 'Message sent. Thank you!'
     }
-  },
-
-  nuxtIcon: {
-    aliases: {
-      key-244: '',
-      key-106: ''
-    }
   }
 })
